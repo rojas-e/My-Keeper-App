d9508c86ce0df64a2929083c56c4cf425e23fcb5
Jonathan Rojas <jonrojas.dev@gmail.com>
2021-05-01 18:18:22 -0700

    add event listener to title

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -7,10 +7,14 @@ function CreateArea(props) {
     content: ''
   });
 
+  const handleChange = () => {
+    console.log('button clicked!')
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
+        <input onChange={handleChange} name="title" placeholder="Title" />
         <textarea name="content" placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
